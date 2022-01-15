d3565aba4c23b4ba41a23b0c56d486d63013f5b1
bedirhan23 <72863715+bedirhan23@users.noreply.github.com>
2022-01-16 01:23:07 +0300

    Update isu_filemanager.c

--- isu_filemanager.c
+++ isu_filemanager.c
@@ -47,3 +47,25 @@ int main(int argc, char* argv[]){
         remove(argv[2]);
     }
 }
+
+/* 
+#include <stdio.h>
+
+void main()
+{
+	int status;
+	char fname[20];
+	printf("\n\n Remove a file from the disk :\n");
+	printf("----------------------------------\n"); 	
+	printf(" Input the name of file to delete : ");
+	scanf("%s",fname);
+	status=remove(fname);
+	if(status==0)
+	{
+		printf(" The file %s is deleted successfully!!\n\n",fname);
+	}
+	else
+	{
+		printf(" This file is can not be deleted %s\n\n",fname);
+	}
+}*/
