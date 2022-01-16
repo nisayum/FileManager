340c32b148b59c5e44d5ac157c98f73c63ed550a
nisayum <83461429+nisayum@users.noreply.github.com>
2022-01-16 19:53:28 +0300

    Update isu_filemanager.c

--- isu_filemanager.c
+++ isu_filemanager.c
@@ -1,81 +1,92 @@
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]){
-
-    //if strcmp(argv[1], "--help") == 0 
+int main(int argc, char* argv[]) {
+    //if(strcmp(argv[1],"--help") == 0)
     FILE *fp;
-
-    if strcmp(argv[argc-1], "/h") == 0 {
-
-        if strcmp(argv[1], "create") == 0 
-        printf("To create a file do \"main display filename.txt\"\n");
-
-        else if strcmp(argv[1], "remove") == 0 
-        printf("To remove a file do \"main remove filenmae.txt\"\n");
-
-        else if strcmp(argv[1], "rename") == 0 
-        printf("To rename a file do \"main rename oldname.txt newname.txt\"\n");
-
-        else if strcmp(argv[1], "copy") == 0 
-        printf("To copy a file do \"main copy source.txt destination.txt\"\n");
-
-        else if strcmp(argv[1], "move") == 0 
-        printf("To move a file do \"main move filename.txt ../files/filename.txt\"\n");
-
-        else if strcmp(argv[1], "append") == 0 
-        printf("To append a file do \"main append filename.txt helloworld\"\n");
-
-        else if strcmp(argv[1], "writeat") == 0 
-        printf("To write at a specific locatipn to file do \"main writeat location filename.txt\"\n");
-
-        else if strcmp(argv[1], "clear") == 0 
-        printf("To clear a file do \"main clear filename.txt\"\n");
-
-        else if strcmp(argv[1], "display") == 0 
-        printf("To display a file do \"main display filename.txt\"\n");
-
-        return 0;
+    if(strcmp(argv[argc-1], "/h") == 0)
+    {
+    	if(strcmp(argv[1], "create") == 0)
+    	printf("To create a file do \"main create filename.txt\"\n");
+    	
+    	else if(strcmp(argv[1], "remove") == 0)
+    	printf("To remove a file do \"main remove filename.txt\"\n");
+    	
+    	else if(strcmp(argv[1], "rename") == 0)
+    	printf("To rename a file do \"main rename oldname.txt newname.txt\"\n");
+    	
+    	else if(strcmp(argv[1], "copy") == 0)
+    	printf("To copy a file do \"main copy source.txt destination.txt\"\n");
+    	
+    	else if(strcmp(argv[1], "move") == 0)
+    	printf("To move a file do \"main move filename.txt ../files/filename.txt\"\n");
+    	
+    	else if(strcmp(argv[1], "append") == 0)
+    	printf("To append a file do \"main append filename.txt helloworld!\"\n");
+    	
+    	else if(strcmp(argv[1], "writeat") == 0)
+    	printf("To write at a specific location to file do \"main writeat location filename.txt\"\n");
+    	
+    	else if(strcmp(argv[1], "clear") == 0)
+    	printf("To clear a file do \"main clear filename.txt\"\n");
+    	
+    	else if(strcmp(argv[1], "display") == 0)
+    	printf("To display a file do \"main display filename.txt\"\n");
+    	
+    	return 0;
     }
-
-    if strcmp(argv[1], "create") == 0 {
+    if(strcmp(argv[1], "create") == 0) {
         fp = fopen(argv[2], "w");
     }
 
-    else if strcmp(argv[1], "remove") == 0 {
+    else if(strcmp(argv[1], "remove") == 0) {
         remove(argv[2]);
     }
 
-    else if strcmp(argv[1], "rename") == 0 {
+    else if(strcmp(argv[1], "rename") == 0) {
         rename(argv[2], argv[3]);
     }
 
-    else if strcmp(argv[1], "copy") == 0 {
+    else if(strcmp(argv[1], "copy") == 0) {
         char ch;
         FILE *dest;
         fp = fopen(argv[2], "r");
 
         dest = fopen(argv[3], "w");
 
-        while (ch = fgetc(fp)) != EOF
+        while ((ch = fgetc(fp)) != EOF)
             fputc(ch, dest);
     }
 
-    else if strcmp(argv[1], "move") == 0 {
+    else if(strcmp(argv[1], "move") == 0) {
         char ch;
         FILE *dest;
         fp = fopen(argv[2], "r");
 
         dest = fopen(argv[3], "w");
 
-        while (ch = fgetc(fp)) != EOF
+        while ((ch = fgetc(fp)) != EOF)
             fputc(ch, dest);
         fclose(fp);
         fp = fopen(argv[2], "w");
         remove(argv[2]);
     }
-}
+
+    else if(strcmp(argv[1], "append") == 0) {
+        char *ap;
+        ap = argv[3];
+        FILE *dest;
+        dest = fopen(argv[2], "a");
+        for(int i = 0; i < strlen(ap); i++)
+        {
+            
+        }
+        
+        fclose(fp);
+    }
+
+   }
 
 /* 
 #include <stdio.h>
